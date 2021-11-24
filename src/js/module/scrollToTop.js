f4566f0f820140b6a9f9163b51632a1fe3c14f5a
sasa-training <ste523887@gmail.com>
2021-11-24 22:30:03 +0900

    5thCommit@separateJS

---
+++ src/js/module/scrollToTop.js
@@ -0,0 +1,30 @@
+export default function (){
+    class ScrollToTop {
+        constructor(btnId,btnPosition ,options = {}) {
+    　// 変数定義場所
+        const defaultOptions = {
+            scroll_to_top_btn: btnId,
+            showBtnPosition: btnPosition,
+        };
+        this.o = Object.assign(defaultOptions, options);
+        this.scroll_to_top_btn = document.querySelector(btnId);
+        this.showBtnPosition = btnPosition;
+        };
+        // メソッド
+        init() { 
+            this.scroll_to_top_btn.addEventListener('click',()=>{
+                window.scroll({top: 0, behavior: 'smooth'});
+            });
+            window.addEventListener('scroll', () => {
+                if(window.pageYOffset > this.showBtnPosition){
+                    this.scroll_to_top_btn.style.opacity = '1';
+                }else if(window.pageYOffset < this.showBtnPosition){
+                    this.scroll_to_top_btn.style.opacity = '0';
+                }
+            });
+        };
+    }
+    const schrollToTop = new ScrollToTop("#page-top",400);
+    schrollToTop.init();
+}
+
