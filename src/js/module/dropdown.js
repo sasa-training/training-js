350a930263df327f66c64afa936cbece5c9103e5
sasa-training <ste523887@gmail.com>
2021-11-30 10:13:06 +0900

    13.1thCommit@chance-dropdown.js

--- src/js/module/dropdown.js
+++ src/js/module/dropdown.js
@@ -1,11 +1,9 @@
 export default class dropdown {
   constructor(root, options = {}) {
     const defaultOptions = {
-      root: document.querySelectorAll(".dropdown-toggle"),
+      root: document.querySelector(".dropdown-toggle"),
       mainMenu: document.querySelectorAll(".main_menu"),
       submenu: document.querySelectorAll("ul.sub_menu"),
-      mouseOver: "mouseover",
-      mouseOut: "mouseout",
       click: "click",
       open: "is-open",
     };
@@ -14,28 +12,19 @@ export default class dropdown {
     this.options = options;
     this.submenu = this.o.submenu;
     this.mainMenu = this.o.mainMenu;
-    this.mouseOver = this.o.mouseOver;
-    this.mouseOut = this.o.mouseOut;
     this.click = this.o.click;
     this.open = this.o.open;
   }
   dropdownItem() {
-    console.log(this.root);
-    this.root.forEach((item) => {
-      item.addEventListener(
-        this.mouseOver,
-        () => {
-          this.submenu[0].classList.add(this.open);
-        },
-        false
-      );
-      item.addEventListener(
-        this.mouseOut,
-        () => {
-          this.submenu[0].classList.remove(this.open);
-        },
-        false
-      );
+    document.addEventListener(this.click, (e) => {
+      if (!e.target.closest(".dropdown-toggle")) {
+        //値を上手く取れないため直接指定
+        this.mainMenu[0].classList.remove(this.open);
+        this.submenu[0].classList.remove(this.open);
+      } else {
+        this.submenu[0].classList.add(this.open);
+        this.mainMenu[0].classList.add(this.open);
+      }
     });
   }
   init() {
